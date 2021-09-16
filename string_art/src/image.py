66e8668f121d8b5855cfddbde1c8a1d55786895e
Nikolay_Ryabichko <nik.ryabichko@gmail.com>
2021-09-16 08:05:20 +0300

    added resize, thumbnail resize, crop

--- string_art/src/image.py
+++ string_art/src/image.py
@@ -10,9 +10,26 @@ class Picture:
     def __init__(self, open_path: str) -> None:
         self.image = Image.open(open_path)
 
-    def save_as_grayscale(self, save_path: str) -> None:
-        img = self.image.convert('L')
-        img.save(save_path)
+    def convert_to_grayscale(self) -> None:
+        self.image = self.image.convert('L')
+
+    def save(self, save_path: str) -> None:
+        self.image.save(save_path)
+
+    def resize(self, new_size: tuple) -> None:
+        self.image = self.image.resize(new_size)
+
+    def thumbnail_resize(self, new_size: tuple) -> None:
+        """ Resize image and keeping it aspect ratios"""
+        self.image.thumbnail(new_size)
+
+    def crop(self, box: tuple) -> None:
+        """ Crop an image
+
+        :param box: the position and size of the cropped region (left, upper, right, lower)
+        :return: None
+        """
+        self.image = self.image.crop(box)
 
 
 if __name__ == '__main__':
@@ -21,7 +38,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Convert to grayscale script")
     parser.add_argument("-o", dest="open_path", required=True)
     parser.add_argument("-s", dest="save_path", required=True)
+    parser.add_argument("--resize", dest="new_size", nargs="+", type=int)
+    parser.add_argument("--t_resize", dest="t_size", nargs="+", type=int)
+    parser.add_argument("--crop", dest="crop_box", nargs="+", type=int)
     args = parser.parse_args()
 
     corgi = Picture(args.open_path)
-    corgi.save_as_grayscale(args.save_path)
+    corgi.convert_to_grayscale()
+    if args.new_size:
+        corgi.resize(tuple(args.new_size))
+    if args.t_size:
+        corgi.thumbnail_resize(tuple(args.t_size))
+    if args.crop_box:
+        corgi.crop(tuple(args.crop_box))
+
+    corgi.save(args.save_path)
